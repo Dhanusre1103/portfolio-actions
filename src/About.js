3d378eab000184f9f7953bc0e2e7cce4bae8a31d
Dhanusree <21ita10@karpagamtech.ac.in>
2024-05-15 13:48:02 +0530

    first commit

--- src/About.js
+++ src/About.js
@@ -25,70 +25,45 @@ const StyledAboutPage = styled.div`
 const Heading = styled.h1`
   font-size: 48px;
   font-family: 'Helvetica', sans-serif;
-  color: #ff4500; /* White font color */
+  color: #ff4500; /* Orange font color */
 `;
-
-
-
-
-
- 
 
 const Paragraph = styled.p`
-flex: 3;
-width: 25%; /* Set width to 100% */
-padding: 20px; /* Adjust padding */
-font-size: 24px; /* Adjust font size */
-border: 1px solid #fff;
-border-radius: 5px;
-background-color: transparent;
-color: #fff;
-min-width: 750px; /* Set minimum width */
+  flex: 3;
+  width: 25%; /* Set width to 25% */
+  padding: 20px; /* Adjust padding */
+  font-size: 24px; /* Adjust font size */
+  border: 1px solid #fff;
+  border-radius: 5px;
+  background-color: transparent;
+  color: #fff;
+  min-width: 750px; /* Set minimum width */
 `;
 
-const ShineButton = styled.a`
+const Button = styled.a`
   position: absolute;
   bottom: 20px; /* Adjust as needed */
+  padding: 10px 20px;
+  font-size: 20px;
+  color: black; /* Black font color */
+  background-color: lavender; /* Lavender background color */
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  animation: ${glow} 2s ease-in-out infinite alternate; /* Apply glow animation */
+`;
+
+const ProjectsButton = styled(Button)`
   right: 20px; /* Adjust as needed */
-  padding: 10px 20px;
-  font-size: 20px;
-  color: black; /* Black font color */
-  background-color: lavender; /* Lavender background color */
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  text-decoration: none;
-  animation: ${glow} 2s ease-in-out infinite alternate; /* Apply glow animation */
 `;
 
-const GitHubButton = styled.a`
-  position: absolute;
-  bottom: 20px; /* Adjust as needed */
+const GitHubButton = styled(Button)`
   right: 150px; /* Adjust as needed */
-  padding: 10px 20px;
-  font-size: 20px;
-  color: black; /* Black font color */
-  background-color: lavender; /* Lavender background color */
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  text-decoration: none;
-  animation: ${glow} 2s ease-in-out infinite alternate; /* Apply glow animation */
 `;
 
-const LinkedInButton = styled.a`
-  position: absolute;
-  bottom: 20px; /* Adjust as needed */
+const LinkedInButton = styled(Button)`
   right: 280px; /* Adjust as needed */
-  padding: 10px 20px;
-  font-size: 20px;
-  color: black; /* Black font color */
-  background-color: lavender; /* Lavender background color */
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  text-decoration: none;
-  animation: ${glow} 2s ease-in-out infinite alternate; /* Apply glow animation */
 `;
 
 const About = () => {
@@ -96,9 +71,9 @@ const About = () => {
     <StyledAboutPage>
       <Heading>About Me</Heading>
       <Paragraph>
-        Hi there! I'm DHANUSREE S, Currently persuing in B.TECH.IT and eager to dive into the IT world.  I'm passionate about technology and ready to learn. I bring fresh perspectives, adaptability, and a hunger to contribute to innovative projects. Let's connect and explore opportunities together!
+        Hi there! I'm DHANUSREE S, currently pursuing a B.TECH in IT and eager to dive into the IT world. I'm passionate about technology and ready to learn. I bring fresh perspectives, adaptability, and a hunger to contribute to innovative projects. Let's connect and explore opportunities together!
       </Paragraph>
-      <ShineButton href="/projects">Projects</ShineButton>
+      <ProjectsButton href="/projects">Projects</ProjectsButton>
       <GitHubButton href="https://github.com/Dhanusre" target="_blank">GitHub</GitHubButton>
       <LinkedInButton href="https://www.linkedin.com/in/dhanusree-s-kit-b52a23265" target="_blank">LinkedIn</LinkedInButton>
     </StyledAboutPage>
